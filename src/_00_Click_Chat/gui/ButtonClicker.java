eb2039a41b9e9f91a3a4326c7317bf7b8cd6ca0b
Aarit Jayaswal <aaritrj@gmail.com>
2025-05-12 18:30:59 -0700

    Resizing issues with text positioning

--- src/_00_Click_Chat/gui/ButtonClicker.java
+++ src/_00_Click_Chat/gui/ButtonClicker.java
@@ -1,5 +1,8 @@
 package _00_Click_Chat.gui;
 
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -14,8 +17,8 @@ import javax.swing.JTextField;
 import _00_Click_Chat.networking.Client;
 import _00_Click_Chat.networking.Server;
 
-public class ButtonClicker extends JFrame {
-	JButton button = new JButton("Send message");
+public class ButtonClicker extends JFrame implements KeyListener {
+	//JButton button = new JButton("Send message");
 	JTextField jetf = new JTextField("Replace with message");
 	JLabel jell = new JLabel("");
 	ArrayList<JLabel> jells = new ArrayList<JLabel>();
@@ -23,6 +26,7 @@ public class ButtonClicker extends JFrame {
 	Server server;
 	Client client;
 	String connectmsg = "not set";
+	boolean isserver=true;
 	
 	
 	public static void main(String[] args) {
@@ -38,20 +42,20 @@ public class ButtonClicker extends JFrame {
 			System.out.println("test 2");
 			setTitle("SERVER");
 			//JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
-			button.addActionListener((e)->{
+			/*button.addActionListener((e)->{
 				server.sendClick();
-			});
-			 
+			});*/
+			jetf.addKeyListener(this);
 			add(jetf);
 			jells.add(new JLabel());
 			add(jells.get(0));
-			add(button);
+			//add(button);
 			jells.get(0).setSize(400, 15);
 			jells.get(0).setLocation(10, 480);
 			jells.get(0).setText("Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
 			//jell.setLocation(0, 180);
 			jetf.setSize(400, 30);
-			button.setSize(50, 50);
+			//button.setSize(50, 50);
 			this.pack();
 			setVisible(true);
 			setSize(400, 600);
@@ -61,6 +65,7 @@ public class ButtonClicker extends JFrame {
 			
 		}else{
 			setTitle("CLIENT");
+			isserver=false;
 			String ipStr="";
 			try {
 				ipStr = InetAddress.getLocalHost().getHostAddress();
@@ -72,18 +77,20 @@ public class ButtonClicker extends JFrame {
 			int port = Integer.parseInt(prtStr);
 			connectmsg = "Connected to: " +ipStr + " Port: " + prtStr;
 			client = new Client(ipStr, port, this);
-			button.addActionListener((e)->{
+			/*button.addActionListener((e)->{
 				client.sendClick();
-			});
+			});*/
+			this.setPreferredSize(new Dimension(400, 600));
+			jetf.addKeyListener(this);
 			add(jetf);
 			jells.add(new JLabel());
 			add(jells.get(0));
-			add(button);
+			//add(button);
 			jells.get(0).setSize(400, 15);
 			jells.get(0).setLocation(10, 480);
 			//jells.get(0).setText("debug");
 			jetf.setSize(400, 30);
-			button.setSize(50, 50);
+			//button.setSize(50, 50);
 			this.pack();
 			setVisible(true);
 			setSize(400, 600);
@@ -96,46 +103,97 @@ public class ButtonClicker extends JFrame {
 		}
 	} 
 	public void sendMessage(boolean isserver) {
+		System.out.println("SEND MESSAGE");
 		String msg = "MESSAGE NOT SET";
+		
 		if(!isserver) {
 		//msg=jell.getText()+"Client: "+jetf.getText();
 		msg="Client: "+jetf.getText();
 		}else {
-			
+			jells.get(0).setText("");
 			//msg=jell.getText()+"Server: "+jetf.getText();
 			msg="Server: "+jetf.getText();
+		
 		}
 		
 		for (int i = 0; i < jells.size(); i++) {
-			jells.get(i).setLocation(10, this.getSize().height-30-((jells.size()-i)*20));
+			jells.get(i).setLocation(10, this.getSize().height-50-((jells.size()-i)*20));
+			System.out.println(jells.get(i).getText()+" at "+(this.getSize().height-30-((jells.size()-i)*20))+" at "+i);
 		}
-		jells.add(new JLabel());
-		add(jells.get(jells.size()-1));
-		jells.get(jells.size()-1).setSize(400, 15);
-		jells.get(jells.size()-1).setLocation(10, this.getSize().height-30);
-		jells.get(jells.size()-1).setText(jells.size()-2+") "+msg);
-		add(button);
-		//button.setVisible(false);
+		System.out.println(this.getSize().height);
+		System.out.println(this.HEIGHT);
 		
+		
+			jells.add(new JLabel());
+			jells.get(jells.size()-1).setSize(400, 15);
+		jells.get(jells.size()-1).setText(jells.size()-2+") "+msg);
+		add(jells.get(jells.size()-1));
+		jells.get(jells.size()-1).setLocation(10, this.getSize().height-50);
+		
+	
+		/*
+		
+		
+		
+		jells.get(0).setSize(380, 15);
+		jells.get(0).setLocation(10, 			  570);
+		jells.get(0).setText("height-30");
+		jells.get(1).setSize(380, 15);
+		jells.get(1).setLocation(10, 			  this.getSize().height);
+		jells.get(1).setText("height");*/
+		
+		System.out.println(jells.get(jells.size()-1).getText()+" at "+(this.getSize().height));
+		//button.setVisible(false);
+			
+			System.out.println(jells.get(1).getText() + "<---");
 	}
 	
 	
 	public void setMessage(String msg) {
+		if(isserver) {
+			jells.get(0).setText("");
+		}
 		for (int i = 0; i < jells.size(); i++) {
-			jells.get(i).setLocation(10, this.getSize().height-30-((jells.size()-i)*20));
+			jells.get(i).setLocation(10, this.getSize().height-50-((jells.size()-i)*20));
 		}
 		jells.add(new JLabel());
-		add(jells.get(jells.size()-1));
 		jells.get(jells.size()-1).setSize(400, 15);
-		jells.get(jells.size()-1).setLocation(10, this.getSize().height-30);
-		jells.get(jells.size()-1).setText(msg);
-		add(button);
+	jells.get(jells.size()-1).setText(msg);
+	add(jells.get(jells.size()-1));
+	jells.get(jells.size()-1).setLocation(10, this.getSize().height-50);
 	}
+	
+	
+	
 	public String getjell() {
 		System.out.println(jells.get(jells.size()-1).getText());
 		return jells.get(jells.size()-1).getText();
 	}
 	public String getconnectmsg() {
 		return connectmsg;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if(isserver) {
+				server.sendClick();
+			}else {
+				client.sendClick();
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
